41b0aadcbf58fd73fe8aa099a04e476beca73444
Jefferson Gutierrez <jeferguto1@outlook.com>
2021-12-28 17:17:26 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const url = 'https://jsonplaceholder.typicode.com/users'
+  const fetchApi = async() => { 
+    const response = await fetch(url)
+    console.log(response.status)
+  }
+  useEffect( ( ) => {
+    fetchApi()
+  }, [] )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TECHNICAL TEST</h1>
     </div>
   );
 }
