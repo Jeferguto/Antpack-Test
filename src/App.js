2300cb0e82493ae215808491e822c6075424e7fd
Jefferson Gutierrez <jeferguto1@outlook.com>
2021-12-29 00:08:38 -0500

    hooks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const url = 'https://jsonplaceholder.typicode.com/users'
+  const [users, setUsers] = useState()
   const fetchApi = async() => { 
     const response = await fetch(url)
-    console.log(response)
+    /* console.log(response.status) */
+    const responseJSON = await response.json()
+    setUsers (responseJSON)
+    /* console.log(responseJSON) */
   }
   useEffect( ( ) => {
     fetchApi()
   }, [] )
+
   return (
     <div className="App">
       <h1>TECHNICAL TEST</h1>
+      <ol>
+      { !users? 'Actualizando' :
+        users.map( (users,index)=>{
+          return <li>{users.name},{users.email},{users.address}</li>
+        })
+      }
+      </ol>
     </div>
   );
 }
