4473beb56153ec1b0f601079518da1923de001a3
Jefferson Gutierrez <jeferguto1@outlook.com>
2021-12-29 15:09:38 -0500

    last version

--- src/App.js
+++ src/App.js
@@ -1,33 +1,79 @@
-import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  const url = 'https://jsonplaceholder.typicode.com/users'
-  const [users, setUsers] = useState()
-  const fetchApi = async() => { 
-    const response = await fetch(url)
-    /* console.log(response.status) */
-    const responseJSON = await response.json()
-    setUsers (responseJSON)
-    /* console.log(responseJSON) */
+class Table extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      users: [],
+      isLoading: false,
+      isError: false,
+    }
   }
-  useEffect( ( ) => {
-    fetchApi()
-  }, [] )
 
-  return (
-    <div className="App">
-      <h1>TECHNICAL TEST</h1>
-      <ol>
-      { !users? 'Actualizando' :
-        users.map( (users,index)=>{
-          return <li>{users.name},{users.email},{users.address}</li>
-        })
-      }
-      </ol>
-    </div>
-  );
+  async componentDidMount(){
+    this.setState({isLoading: true})
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    /* console.log(response.json()) */
+
+    if(response.ok){
+
+      const users = await response.json()
+      /* console.log(users) */
+      this.setState( { users, isLoading: false} )
+    }else{
+      this.setState( {isError: true, isLoading: false} )
+    }
+  }
+
+  renderTableHeader = () =>{
+    return Object.keys(this.state.users[0]).map(attr => <th key={attr}> 
+    {attr.toUpperCase()}
+    </th>)
+  }
+  
+  renderTableRows = () => {
+    return this.state.users.map(user => {
+      return (
+        <tr key={user.id}>
+          <td>{user.id}</td>
+          <td>{user.name}</td>
+          <td>{user.username}</td>
+          <td>{user.email}</td>
+          <td>{user.address.city}</td>
+          <td>{"empty"}</td>
+          <td>{"empty"}</td>
+          <td>{user.company.name}</td>
+
+        </tr>
+      )
+    })
+  }
+
+  render() {
+    const {users, isLoading, isError} = this.state
+    if (isLoading) {
+      return <div>the data is loading</div>
+    }
+    if (isError) {
+      return <div>¡Ups! an error occurred</div>
+    }
+    return users.length > 0 
+    ? ( 
+      <table>
+        <thead>
+          <tr>
+            {this.renderTableHeader()}
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderTableRows()}
+        </tbody>
+      </table>
+    ) : (
+      <div>no users!</div>
+    )
+  }
 }
 
-export default App;
+export default Table
